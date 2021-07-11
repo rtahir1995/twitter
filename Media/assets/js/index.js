b205c0ad17f55954cd01b01948938f22084d72a0
rtahir1995 <mail.to@tahir-rahimov.com>
2021-07-11 20:58:20 +0400

    Twtiiter Version 3.0 with Sign

--- Media/assets/js/index.js
+++ Media/assets/js/index.js
@@ -17,3 +17,42 @@ $('.popup').on('click', function () {
 $('.popupContent').on('click', function(event) {
     event.stopPropagation();
 });
+
+
+// Birthday form 
+
+function dateBirthDayUpdate () {
+
+    var monthSel = $('#dateBirthMonth').val();
+    var yearSel = $('#dateBirthYear').val();
+    var daySelOp = $('#dateBirthDay');
+    var yearSelOp = $('#dateBirthYear');
+
+
+    var maxYear = new Date().getFullYear();
+    var yearSelected = yearSel;
+    yearSelOp.html(`<option value="0" selected="1" disabled="disabled"></option>`);
+    for (let i = maxYear; i >= 1901; i--) {
+        let sel = yearSelected == i ? ' selected' : '';
+        yearSelOp.append( `<option value="${i}" ${sel}> ${i}  </option>`);
+    }
+
+    
+
+    
+    if (yearSel === null) {
+        yearSel = 2020;
+    };
+    
+    var date = new Date(yearSel, monthSel);
+    date.setDate(0);
+    var maxDays = date.getDate();
+    var daySelected = daySelOp.val();
+    daySelOp.html(`<option value="0" selected="1" disabled="disabled"></option>`);
+    for (let i = 1; i <= maxDays; i++) { 
+        let sel = daySelected == i ? ' selected' : '';
+        daySelOp.append( `<option value="${i}" ${sel}> ${i}  </option>`);
+    };
+}
+
+dateBirthDayUpdate();
